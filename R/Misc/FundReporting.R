eeeb4d2021e4b5f4eff2f1e790e345731f3a117d
williamlfang <william.lian.fang@gmail.com>
2018-06-09 17:30:05 +0800

    针对净值计算统一使用一个函数

--- R/Misc/FundReporting.R
+++ R/Misc/FundReporting.R
@@ -26,13 +26,11 @@ ChinaFuturesCalendar <- fread("./data/ChinaFuturesCalendar/ChinaFuturesCalendar.
 currTradingDay <- ChinaFuturesCalendar[days <= format(Sys.Date(), '%Y%m%d')][nights < format(Sys.Date(), '%Y%m%d')][.N]
 ## =============================================================================
 
-accountInfo <- data.table(accountID = c('TianMi2','TianMi3','YunYang1',
-                                        'SimNow_YY','SimNow_LXO'),
-                          accountCapital = c(1300000,1000000, 2000000,
-                                             1000000, 1000000),
-                          accountName = c('甜蜜2号','甜蜜3号','云扬1号',
-                                          'SimNow_YY', 'SimNow_LXO')
-                          )
+accountAll <- data.table(
+    accountID   = c('TianMi2','TianMi3','YunYang1','YongAnLYB'),
+    accountName = c('甜蜜2号','甜蜜3号','云扬1号','永安林艳彬')
+    )
+
 logPath <- "/home/fl/myData/log/FundReporting"
 
 tempFile <- paste0(logPath,'/',currTradingDay[1, days],'_fund.txt')
@@ -41,189 +39,210 @@ if (file.exists(tempFile)) file.remove(tempFile)
 if (file.exists(tempFile)) file.remove(tempFile)
 
 
+
+navCalculating <- function(fundID) {
+    mysql <- mysqlFetch(fundID, host = '192.168.1.135')
+
+    ## 如果右 nav 这个数据表
+    ## 说明有对理财数据进行爬虫处理，
+    ## 如　YunYang1 基金
+    dtNav <- dbGetQuery(mysql, 
+        "select * from nav order by TradingDay") %>% 
+        as.data.table()
+    ## 分开讨论两种情况
+    
+    ## 由交易系统保存的信息
+    dtAccount <- dbGetQuery(mysql, 
+        "select * from accountInfo order by TradingDay") %>% 
+        as.data.table()
+
+    if (nrow(dtNav) == 0) {
+        ## 基金手续费
+        fee <- dbGetQuery(mysql, "select * from fee order by TradingDay") %>% 
+            as.data.table()
+        dtAccount <- merge(dtAccount, fee, by = 'TradingDay', all.x = TRUE)
+
+        ## 基金申购与赎回
+        funding <- dbGetQuery(mysql, "select * from funding order by TradingDay") %>% 
+            as.data.table()
+        dtAccount <- merge(dtAccount, funding, by = 'TradingDay', all.x = TRUE)
+
+        ## --------------------
+        ## 开始处理 nav 净值计算
+        ## --------------------
+        dtNav <- dtAccount[, .(
+            TradingDay, accountID, updateTime, 
+            balance, deposit, withdraw,
+            flowCapital, banking, fee,
+            shares = ifelse(is.na(shares.y), 0, shares.y)
+            )]
+        dtNav[, ":="(
+            assets = balance + flowCapital + banking + cumsum(fee),
+            shares = cumsum(shares)
+            )]
+        dtNav[, nav := assets / shares]
+
+    } else {
+        dtNav <- merge(dtNav, 
+                       dtAccount[, .(TradingDay, accountID, updateTime, 
+                                     balance, deposit, withdraw, fee)],
+                       by = 'TradingDay', all.x = TRUE)
+        dtNav <- dtNav[, .(TradingDay, accountID, updateTime,
+                           balance, deposit, withdraw,
+                           flowCapital = Currency,
+                           banking = Bank,
+                           fee,
+                           assets = Futures + Currency + Bank,
+                           shares = Shares)]
+        dtNav[, nav := assets / shares]
+    }
+
+    dtNav[, TradingDay := as.Date(TradingDay)] %>% 
+        .[, rtn := c(0, diff(nav)/.SD[1:(.N-1), nav])] %>%
+        .[, ":="(
+            maxNav = cummax(nav),
+            DD  = nav / cummax(nav) - 1,
+            maxDD = cummin((nav - cummax(nav)) / cummax(nav))
+        )]
+
+    ## ----------------------------------------
+    for ( conns in dbListConnections(MySQL()) ) dbDisconnect(conns)
+    ## ----------------------------------------
+
+    ## -----------
+    return(dtNav)
+    ## -----------
+}
+
 ## =============================================================================
 ## i = 1
-fetchFund <- function(i, author = FALSE) {
-  mysql <- mysqlFetch(accountInfo[i,accountID], host = '192.168.1.135')
+fetchFund <- function(fundID, author = FALSE) {
+    ## ----------------------------
+    dtNav <- navCalculating(fundID)
+    ## ----------------------------
 
-  reportAccount <- dbGetQuery(mysql,
-    "select * from report_account_history
-     order by TradingDay") %>%
-    as.data.table()
+    ## -------------------------------------------------------------------------
+    fundPerformance <- data.table(
+        基金名称 = accountAll[accountID == fundID, accountName],
+        期货账户 = prettyNum(dtNav[.N, balance + sum(fee)], big.mark = ','),
+        现货账户 = prettyNum(dtNav[.N, flowCapital], big.mark = ','),
+        银行存款 = prettyNum(dtNav[.N, banking], big.mark = ','),
+        账户总额 = prettyNum(dtNav[.N, assets], big.mark = ','),
+        今日盈亏 = ifelse(nrow(dtNav) > 1,
+                      prettyNum(
+                        dtNav[.N, assets - (deposit - withdraw)] -
+                        dtNav[.N-1, assets - (deposit - withdraw)], big.mark = ','),
+                          0),
+        收益波动 = dtNav[.N, paste0(as.character(round(rtn * 100),2),'%')],
+        基金净值 = dtNav[.N, round(nav,4)]
+        )
+    ## -------------------------------------------------------------------------
 
-  if (nrow(reportAccount) == 0) return(NULL)
+    mysql <- mysqlFetch(fundID, host = '192.168.1.135')
 
-  fee <- dbGetQuery(mysql, 'select * from fee') %>% as.data.table()
+    ## -------------------------------------------------------------------------
+    positionInfo <- dbGetQuery(mysql,
+        "select * from positionInfo") %>% as.data.table() %>%
+        .[, .(volume = .SD[, sum(volume)])
+          , by = c('strategyID','InstrumentID', 'direction')] %>%
+        .[order(InstrumentID)]
+    # print(positionInfo)
+    ## -------------------------------------------------------------------------
 
-  if (nrow(fee) != 0) {
-    for (j in 1:nrow(fee)) {
-      tempTradingDay <- fee[j, TradingDay]
-      reportAccount[TradingDay >= tempTradingDay,
-            totalMoney := totalMoney + fee[TradingDay == tempTradingDay, sum(Amount)]]
+    ## -------------------------------------------------------------------------
+    tradingInfo <- dbGetQuery(mysql, paste(
+        "select * from tradingInfo
+        where TradingDay = ", currTradingDay[1, days])) %>%
+        as.data.table() %>%
+        .[, TradingDay := NULL]
+    # print(tradingInfo)
+    ## -------------------------------------------------------------------------
+
+
+    ## -------------------------------------------------------------------------
+    failedInfo <- dbGetQuery(mysql,
+        "select * from failedInfo") %>% as.data.table()
+    failedInfo[offset == '平仓', direction := ifelse(direction == 'long','short','short')]
+    failedInfo[, offset := NULL]
+    setcolorder(failedInfo, c('TradingDay', 'strategyID', 'InstrumentID', 'direction','volume'))
+    # print(positionInfo)
+    ## -------------------------------------------------------------------------
+
+    ## ---------------------------------------------------------------------------
+    ## 写入 log
+    tempFile <- paste0(logPath,'/',currTradingDay[1, days],'_fund.txt')
+    if (!grepl('SimNow', accountAll[i,accountID])) {
+        sink(tempFile, append = TRUE)
+        cat("## ----------------------------------- ##")
+        cat('\n')
+        write.table(as.data.frame(t(fundPerformance)), tempFile
+                    , append = TRUE, col.names = FALSE
+                    , sep = ' :==> ')
+        if (!is.na(grep('SimNow', accountAll$accountID)[1])) {
+            if (i == grep('SimNow', accountAll$accountID)[1]-1) {
+                cat("## ----------------------------------- ##\n")
+                cat("\n## ----------------------------------- ##\n")
+                cat("## @william")
+            }
+        }
     }
-  }
 
-  nav <- dbGetQuery(mysql,"select * from nav order by TradingDay") %>%
-    as.data.table()
-
-  ## ---------------------------------------------------------------------------
-  if (nrow(nav) != 0) {
-    if (! currTradingDay[1, as.character(ymd(days))] %in% nav[,TradingDay]) return(NULL)
-    currNav <- nav[TradingDay == currTradingDay[1, ymd(days)]]
-    fund <- data.table(基金名称 = accountInfo[i, accountName]
-                       #,基金经理 = accountInfo[i, managerID]
-                       ,期货金额 = currNav[1, Futures],
-                       现货金额 = currNav[1, Currency],
-                       银行存款 = currNav[1, Bank],
-                       账户总额 = currNav[1, Assets],
-                       今日盈亏 = ifelse(nrow(nav) > 1,
-                                         nav[.N, Assets] -
-                                         nav[.N-1, Assets],
-                                         0),
-                       收益波动 = currNav[1, paste0(as.character(GrowthRate * 100),'%')],
-                       基金净值 = currNav[1, NAV]
-    )
-  } else {
-    fundingInfo <- dbGetQuery(mysql,"select * from funding") %>%
-                as.data.table()
-
-    if (nrow(fundingInfo) == 0) {
-        fundChg <- ifelse(nrow(reportAccount) > 1,
-                          round((reportAccount[.N, allMoney] -
-                                   reportAccount[.N-1, allMoney]) /
-                                  reportAccount[.N-1, allMoney], 4),
-                          0)
-    } else {
-        fundChg <- ifelse(nrow(reportAccount) > 1,
-                          (reportAccount[.N, allMoney] / fundingInfo[, sum(shares)]) / 
-                          (reportAccount[.N-1, allMoney] / 
-                            fundingInfo[TradingDay < reportAccount[.N-1, TradingDay], sum(shares)]) - 1
-                          0)
-    }
-
-    fund <- data.table(基金名称 = accountInfo[i, accountName]
-                           #,基金经理 = accountInfo[i, managerID]
-                           ,期货金额 = reportAccount[.N, totalMoney],
-                           理财金额 = reportAccount[.N, flowMoney],
-                           银行存款 = '--',
-                           账户总额 = reportAccount[.N, allMoney],
-                           今日盈亏 = ifelse(nrow(reportAccount) > 1,
-                                         reportAccount[.N, allMoney] -
-                                           reportAccount[.N-1, allMoney],
-                                         0),
-                           收益波动 = paste0(as.character(round(fundChg,4) * 100),'%'),
-                           基金净值 = round(reportAccount[.N, allMoney] /
-                                          accountInfo[i, accountCapital], 4)
-    )
-  }
-
-  # print(t(fund))
-  ## ---------------------------------------------------------------------------
-
-  ## ---------------------------------------------------------------------------
-  positionInfo <- dbGetQuery(mysql,
-    "select * from positionInfo") %>% as.data.table() %>%
-    .[, .(volume = .SD[, sum(volume)])
-      , by = c('strategyID','InstrumentID', 'direction')] %>%
-    .[order(InstrumentID)]
-  # print(positionInfo)
-  ## ---------------------------------------------------------------------------
-
-  ## ---------------------------------------------------------------------------
-  tradingInfo <- dbGetQuery(mysql, paste(
-    "select * from tradingInfo
-    where TradingDay = ", currTradingDay[1, days])) %>%
-    as.data.table() %>%
-    .[, TradingDay := NULL]
-  # print(tradingInfo)
-  ## ---------------------------------------------------------------------------
-
-
-  ## ---------------------------------------------------------------------------
-  failedInfo <- dbGetQuery(mysql,
-    "select * from failedInfo") %>% as.data.table()
-  failedInfo[offset == '平仓', direction := ifelse(direction == 'long','short','short')]
-  failedInfo[, offset := NULL]
-  setcolorder(failedInfo, c('TradingDay', 'strategyID', 'InstrumentID', 'direction','volume'))
-  # print(positionInfo)
-  ## ---------------------------------------------------------------------------
-
-  ## ---------------------------------------------------------------------------
-  ## 写入 log
-  tempFile <- paste0(logPath,'/',currTradingDay[1, days],'_fund.txt')
-  if (!grepl('SimNow', accountInfo[i,accountID])) {
+    ## ===========================================================================
+    tempFile <- paste0(logPath,'/',currTradingDay[1, days],'.txt')
     sink(tempFile, append = TRUE)
     cat("## ----------------------------------- ##")
     cat('\n')
-    write.table(as.data.frame(t(fund)), tempFile
+    cat(paste0('## >>>>>>>>> ',fundID))
+    cat('\n')
+    cat("## ----------------------------------- ##")
+    cat('\n')
+    write.table(as.data.frame(t(fundPerformance)), tempFile
                 , append = TRUE, col.names = FALSE
                 , sep = ' :==> ')
-    if (!is.na(grep('SimNow', accountInfo$accountID)[1])) {
-      if (i == grep('SimNow', accountInfo$accountID)[1]-1) {
-        cat("## ----------------------------------- ##\n")
+    cat('\n')
+    if (nrow(positionInfo) != 0) {
+        cat("## ----------------------------------- ##")
+        cat("\n## 今日持仓信息 ##")
+        cat('\n')
+        print(positionInfo)
+        cat('\n')
+    }  
+
+    if (nrow(failedInfo) != 0) {
+        cat("## ----------------------------------- ##")
+        cat("\n## 未平仓信息 ##")
+        cat('\n')
+        print(failedInfo)
+        cat('\n')
+    }
+
+    ## ===========================================================================
+    ## 不看交易记录
+    # if (nrow(tradingInfo) != 0) {
+    #   write.table("## -------------------------------------- ##", tempFile
+    #               , append = TRUE, col.names = FALSE, row.names = FALSE)
+    #   cat("## 今日交易记录 ##")
+    #   cat('\n')
+    #   print(tradingInfo)
+    #   cat('\n')
+    # }
+    cat("## ----------------------------------- ##\n")
+    ## ===========================================================================
+    if (author) {
         cat("\n## ----------------------------------- ##\n")
         cat("## @william")
-      }
     }
-  }
-
-  ## ===========================================================================
-  tempFile <- paste0(logPath,'/',currTradingDay[1, days],'.txt')
-  sink(tempFile, append = TRUE)
-  cat("## ----------------------------------- ##")
-  cat('\n')
-  cat(paste0('## >>>>>>>>> ',accountInfo[i,accountName]))
-  cat('\n')
-  cat("## ----------------------------------- ##")
-  cat('\n')
-  write.table(as.data.frame(t(fund)), tempFile
-              , append = TRUE, col.names = FALSE
-              , sep = ' :==> ')
-  cat('\n')
-  if (nrow(positionInfo) != 0) {
-    # write.table("## ---------------------------------- ##", tempFile
-    #             , append = TRUE, col.names = FALSE, row.names = FALSE)
-    cat("## ----------------------------------- ##")
-    cat("\n## 今日持仓信息 ##")
-    cat('\n')
-    print(positionInfo)
-    cat('\n')
-  }
-
-  if (nrow(failedInfo) != 0) {
-    # write.table("## ---------------------------------- ##", tempFile
-    #             , append = TRUE, col.names = FALSE, row.names = FALSE)
-    cat("## ----------------------------------- ##")
-    cat("\n## 未平仓信息 ##")
-    cat('\n')
-    print(failedInfo)
-    cat('\n')
-  }
-
-  ## ===========================================================================
-  ## 不看交易记录
-  # if (nrow(tradingInfo) != 0) {
-  #   write.table("## -------------------------------------- ##", tempFile
-  #               , append = TRUE, col.names = FALSE, row.names = FALSE)
-  #   cat("## 今日交易记录 ##")
-  #   cat('\n')
-  #   print(tradingInfo)
-  #   cat('\n')
-  # }
-  cat("## ----------------------------------- ##\n")
-  ## ===========================================================================
-  if (author) {
-    cat("\n## ----------------------------------- ##\n")
-    cat("## @william")
-  }
 }
 ## =============================================================================
 
-for (i in 1:nrow(accountInfo)) {
-  # print(i)
-  if (i < nrow(accountInfo)) {
-    fetchFund(i)
-  } else {
-    fetchFund(i, author = TRUE)
-  }
+for (i in 1:nrow(accountAll)) {
+    # print(i)
+    if (i < nrow(accountAll)) {
+        fetchFund(accountAll[i, accountID])
+    } else {
+        fetchFund(accountAll[i, accountID], author = TRUE)
+    }
 }
+## ----------------------------------------
+for ( conns in dbListConnections(MySQL()) ) dbDisconnect(conns)
+## ----------------------------------------
